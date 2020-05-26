227ff9354c3ab44935f8938f165746cb77950698
An5H <46046376+An5H@users.noreply.github.com>
2020-05-26 18:48:25 +0530

    Create Kadane'sAlgorithm.java

---
+++ Kadane'sAlgorithm.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Kadane obj = new Kadane();
+		    
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Kadane{
+    
+    // Function to find subarray with maximum sum
+    // arr: input array
+    // n: size of array
+    int maxSubarraySum(int arr[], int n){
+        int max = 0, maxSoFar = Integer.MIN_VALUE;
+        for(int i: arr){
+            max = Math.max(i, max+i);
+            maxSoFar = Math.max(maxSoFar, max);
+        }
+        return maxSoFar;
+    }
+}
+
